f60850d85fe43aaea7d9baee0ef59e962226a8a6
bruno <truongtritin.bee@gmail.com>
2023-04-21 21:36:32 +0700

    asdasddas

--- src/components/Movies/MovieListContainer.jsx
+++ src/components/Movies/MovieListContainer.jsx
@@ -2,7 +2,7 @@ import MoviesList from "./MoviesList";
 
 export function MovieListContainer({ genresId }) {
   return (
-    <div className="h-1/4 md:h-2/6 xl:h-3/6  relative flex-shrink-0 overflow-auto w-full items-center justify-between gap-3 rounded-xl">
+    <div className="h-1/4 md:h-4/6 xl:h-5/6  relative flex-shrink-0 w-full items-center justify-between gap-3 rounded-xl">
       <MoviesList genresId={genresId} />
     </div>
   );
--- src/components/Movies/MoviesList.jsx
+++ src/components/Movies/MoviesList.jsx
@@ -7,7 +7,7 @@ export function MovieInfo({ filmTilte, backDrop, movieID }) {
     return `https://image.tmdb.org/t/p/original${poster_path} `;
   };
   return (
-    <div className="w-3/4 relative sm:w-3/4 md:w-1/4 xl:w-1/6 flex-shrink-0 rounded-xl">
+    <div className="w-3/4 h-full relative sm:w-3/4 md:w-1/4 xl:w-1/4 flex-shrink-0 rounded-xl">
       <Link to={`/detail/${movieID}`}>
         <img
           className="absolute object-cover w-full h-full rounded-xl"
@@ -15,13 +15,13 @@ export function MovieInfo({ filmTilte, backDrop, movieID }) {
         ></img>
       </Link>
       <img
-        className="absolute h-10 top-3 rounded-xl"
+        className="absolute h-10 top-3 rounded-xl object-cover object-center"
         src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo-2006.png"
       ></img>
       <div className="absolute w-full flex items-center justify-center rounded-3xl h-1/4 bottom-0 bg-opacity-5">
-        {/* <h1 className="text-white z-10 font-medium bottom-3  h-full left-3 text-sm md:text-lg xl:text-xl">
-          {filmTilte}
-        </h1> */}
+        <h1 className="text-white z-10 font-medium absolute text-sm md:text-lg xl:text-sm">
+          {filmTilte.slice}
+        </h1>
         {/* <div className="w-full h-full absolute bg-black blur bg-opacity-20"></div> */}
       </div>
     </div>
@@ -49,7 +49,7 @@ function MoviesList({ genresId }) {
         return (
           <MovieInfo
             movieID={movie.id}
-            backDrop={movie.backdrop_path || movie.poster_path}
+            backDrop={movie.poster_path}
             filmTilte={movie.original_title}
           />
         );
